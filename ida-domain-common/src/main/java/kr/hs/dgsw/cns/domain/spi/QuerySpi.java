3ae67dc9e8540b3d40ed6a1ab2bc8fc40c7f2c65
iqpizza6349 <iqpizza6349@gmail.com>
2023-04-02 11:43:03 +0900

    :memo: :: add ida-domain-common module README.MD

--- ida-domain-common/src/main/java/kr/hs/dgsw/cns/domain/spi/QuerySpi.java
+++ ida-domain-common/src/main/java/kr/hs/dgsw/cns/domain/spi/QuerySpi.java
@@ -2,8 +2,20 @@ package kr.hs.dgsw.cns.domain.spi;
 
 import java.util.Optional;
 
+/**
+ * 도매인 레이어에서 쿼리(저수준 모듈)에서 사용될 쿼리 SPI
+ * @param <T> 도메인 
+ * @param <ID> 도메인의 식별 클래스
+ */
 public interface QuerySpi<T, ID> {
 
+    /**
+     * {@link ID}를 식별값으로 갖는 도메인 클래스를 찾습니다.
+     * 만일 존재하지 않을 경우, {@link Optional#empty()}를
+     * 반환합니다.
+     * @param id 식별값
+     * @return {@link ID}를 식별 타입으로 갖는 도메인 클래스를 반환합니다.
+     */
     Optional<T> findById(ID id);
 
 }
--- ida-domain-common/src/main/java/kr/hs/dgsw/cns/domain/value/PhoneNumber.java
+++ ida-domain-common/src/main/java/kr/hs/dgsw/cns/domain/value/PhoneNumber.java
@@ -5,15 +5,16 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Objects;
 
+/**
+ * 여러 애그리거트에서 공통적으로 전화번호라는 필드가 사용되기에
+ * 따로 Value-Class로 설계된 클래스 <br>
+ * 해당 클래스는 Value-Class이므로 불변성을 가지고 있습니다.
+ */
 @Getter
 @RequiredArgsConstructor
 public class PhoneNumber {
 
     private final String number;
-
-    public PhoneNumber changePhoneNumber(String contact) {
-        return new PhoneNumber(contact);
-    }
 
     @Override
     public boolean equals(Object obj) {
