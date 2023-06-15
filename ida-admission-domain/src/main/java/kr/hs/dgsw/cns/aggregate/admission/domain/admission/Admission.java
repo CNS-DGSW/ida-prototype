345018948d3cf0d3fa7c7b2e325dfaa1f55c903f
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-16 08:55:58 +0900

    :card_file_box: :: add new properties that I(iqpizza6349) forget

--- ida-admission-domain/src/main/java/kr/hs/dgsw/cns/aggregate/admission/domain/admission/Admission.java
+++ ida-admission-domain/src/main/java/kr/hs/dgsw/cns/aggregate/admission/domain/admission/Admission.java
@@ -16,14 +16,30 @@ public class Admission {
 
     private Long id;
 
+    // 지원생
     private AdmissionApplicant applicant;
 
+    // 자소서 및 학업계획서
     private Document document;
 
+    // 제출 현황
     private AdmissionStatus status;
 
+    // 지원, 1차, 2차 상태
     private Progress progress;
 
+    // 점수
     private Score score;
 
+    public void updateDocument(Document document) {
+        this.document = document;
+    }
+
+    public void updateStatus(AdmissionStatus status) {
+        this.status = status;
+    }
+
+    public void updateProgress(Progress progress) {
+        this.progress = progress;
+    }
 }
