5a0bf69e697a5696c0842c64d4e37a5acd9e6d03
jiseok419 <pyosik0419@gmail.com>
2023-09-14 00:39:08 +0900

    :ambulance: :: (#12) Move packages and resolve join issues

--- ida-secondary-infrastructure/src/main/java/kr/hs/dgsw/cns/aggregate/secondary/entity/value/ExamineeVO.java
+++ ida-secondary-infrastructure/src/main/java/kr/hs/dgsw/cns/aggregate/secondary/entity/value/ExamineeVO.java
@@ -1,6 +1,8 @@
 package kr.hs.dgsw.cns.aggregate.secondary.entity.value;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import kr.hs.dgsw.cns.aggregate.admission.entity.admission.AdmissionEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +20,8 @@ public class ExamineeVO implements Serializable {
     @Serial
     private static final long serialVersionUID = -2238420653727913619L;
 
+    @OneToOne
+    @JoinColumn(name = "admission_id")
     private AdmissionEntity admission;
 
     private Long examCode;
